8915180cac6cb95125554c3cc3bbf9eb076e9291
Zephania Eliah Mundekesye <zmunde5@live.com>
2022-11-09 23:26:33 +0300

    added test project and solution folders , added moq nuget package for data mocking and fluent assertion nuget package

---
+++ Job.Test/CandidatesControllerTest.cs
@@ -0,0 +1,15 @@
+namespace Job.Test
+{
+    public class CandidatesControllerTest
+    {
+        [Fact]
+        public void GetCandidates_WithEmptyList_ReturnNotFound()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
