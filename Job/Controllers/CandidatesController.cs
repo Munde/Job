a85eafb041c592d062f0f6434c8a88f5e0fbf134
Zephania Eliah Mundekesye <zmunde5@live.com>
2022-11-09 17:43:52 +0300

    -implemented candidate method to post record, to read records and deleted records

--- Job/Controllers/CandidatesController.cs
+++ Job/Controllers/CandidatesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Job.Domains.Candidates;
+using Job.Repositories;
+using Job.Shared.Models.Candidates;
+
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Job.Controllers
@@ -7,9 +11,49 @@ namespace Job.Controllers
     [ApiController]
     public class CandidatesController : ControllerBase
     {
-        public CandidatesController()
-        {
+        private readonly ICandidateRepository repository;
 
+        public CandidatesController(ICandidateRepository repository)
+        {
+            repository = repository;
         }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async ValueTask<ActionResult<IEnumerable<Candidate>>> GetAsync(
+            [FromQuery] int page = 1,
+            [FromQuery] int size = 5)
+        {
+            if(page <= 1)
+                throw new ArgumentException("Invalid page number provided", nameof(page));
+
+            if(size < 1)
+                throw new ArgumentException("Invalid page size provided", nameof(page));
+
+            var candidates = await repository.GetAllAsync(page, size);
+
+            if(!candidates.Any())
+                return NotFound();
+
+            return Ok(candidates);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesErrorResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async ValueTask<IActionResult> GetAsync(Guid id)
+        {
+            var candidate = await repository.GetAsync(id);
+
+            if(candidate is null) 
+                return NotFound();
+
+            return Ok(candidate);
+        }
+
+
+
+
     }
 }
