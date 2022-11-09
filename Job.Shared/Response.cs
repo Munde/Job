6e654583942f1aef04208e37f870a545caa10496
Zephania Eliah Mundekesye <zmunde5@live.com>
2022-11-09 15:52:06 +0300

    Added response class and candidate dto for easy data transfer from one part to another

---
+++ Job.Shared/Response.cs
@@ -0,0 +1,13 @@
+﻿namespace Job.Shared
+{
+    public record class Response
+    {
+        public Response(bool isValid,string message)
+        {
+            IsValid = isValid;
+            Message = message;
+        }
+        public bool IsValid { get; set; }
+        public string Message { get; set; }
+    }
+}
