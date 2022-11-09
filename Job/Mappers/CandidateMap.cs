94294d02041c09e95b3bd5af76e8c6bb1fc3d523
Zephania Eliah Mundekesye <zmunde5@live.com>
2022-11-09 16:24:27 +0300

    Added CsvHelper nuget for reading and creating records in csv and defined a mapper to help mapping fields to and from the csv

---
+++ Job/Mappers/CandidateMap.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper.Configuration;
+
+using Job.Shared.Models.Candidates;
+
+namespace Job.Mappers
+{
+    public class CandidateMap:ClassMap<Candidate>
+    {
+        public CandidateMap()
+        {
+            Map(x => x.Id).Name("Id");
+            Map(x => x.Firstname).Name("Firstname");
+            Map(x => x.Lastname).Name("Lastname");
+            Map(x => x.Email).Name("Email");
+            Map(x => x.Phone).Name("Phone");
+            Map(x => x.CallTimeInterval).Name("CallTimeInterval");
+            Map(x => x.LinkedInProfile).Name("LinkedInProfile");
+            Map(x => x.GithubLink).Name("GithubLink");
+            Map(x => x.Comment).Name("Comment");
+            Map(x => x.CreatedAt).Name("CreatedAt");
+        }
+    }
+}
