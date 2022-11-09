4377ae75d35dc7c4f7795b461fa1b9be43c11e44
Zephania Eliah Mundekesye <zmunde5@live.com>
2022-11-09 23:36:17 +0300

    added a test to check if no element is  squence returns not found and it passes

--- Job.Test/CandidatesControllerTest.cs
+++ Job.Test/CandidatesControllerTest.cs
@@ -1,15 +1,34 @@
+using FluentAssertions;
+
+using Job.Controllers;
+using Job.Domains.Candidates;
+using Job.Shared.Models.Candidates;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+
 namespace Job.Test
 {
     public class CandidatesControllerTest
     {
         [Fact]
-        public void GetCandidates_WithEmptyList_ReturnNotFound()
+        public async Task GetCandidates_WithEmptyList_ReturnNotFound()
         {
             //Arrange
+            int page = 1,
+                size = 5;
+            var repositoryStub = new Mock<ICandidateRepository>();
+            repositoryStub.Setup(repo => repo.GetAllAsync(page, size))
+                .ReturnsAsync(new List<Candidate>());
 
+            var controller = new CandidatesController(repositoryStub.Object);
             //Act
+            var actionResult = await controller.Get(page, size);
 
             //Assert
+            var result = actionResult.Result;
+            result.Should().BeOfType<NotFoundResult>();
         }
     }
 }
