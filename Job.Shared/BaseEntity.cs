81b1e4583dc7b055de6d9fb85c810d0ca1d3e716
Zephania Eliah Mundekesye <zmunde5@live.com>
2022-11-09 15:45:11 +0300

    -Added a shared class library where all the entitity will be located and all the data transfer objects - added candidate model - added abstract base entity class that be inherited by other classes

---
+++ Job.Shared/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Job.Shared
+{
+    /**
+     * base class that will be inhrent by all
+     * classes that my be need to access datastore
+     */
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now;
+    }
+}
---
+++ Job.Shared/Models/Candidates/Candidate.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Job.Shared.Models.Candidates
+{
+    public class Candidate :BaseEntity
+    {
+        [StringLength(50)]
+        public required string Firstname { get; set; }
+        [StringLength(50)]
+        public required string Lastname { get; set; }
+
+        [StringLength(50), EmailAddress]
+        public required string Email { get; set; }
+
+        [StringLength(20)]
+        public string? Phone { get; set; }
+
+        [StringLength(50)]
+        public string? CallTimeInterval { get; set; }
+
+        [StringLength(100)]
+        public string? LinkedInProfile { get; set; }
+
+        [StringLength(100)]
+        public string? GithubLink { get; set; }
+        public string? Comment { get; set; }
+    }
+}
