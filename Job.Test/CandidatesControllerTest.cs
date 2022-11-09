06d95a61a3fe800deaeb1fdff527ee2cc00da948
Zephania Eliah Mundekesye <zmunde5@live.com>
2022-11-09 23:54:40 +0300

    added a test to check if the controller return the candidates and added a custom candidate generator

--- Job.Test/CandidatesControllerTest.cs
+++ Job.Test/CandidatesControllerTest.cs
@@ -30,5 +30,52 @@ namespace Job.Test
             var result = actionResult.Result;
             result.Should().BeOfType<NotFoundResult>();
         }
+
+        [Fact]
+        public async Task GetCandidates_FromListOfExistingCandidates_ReturnsCandidates()
+        {
+            //Arrange
+            int page = 1,
+                size = 5;
+            var candidates = new List<Candidate>()
+            {
+                RandomCandidate(),
+                RandomCandidate(),
+                RandomCandidate(),
+                RandomCandidate(),
+                RandomCandidate(),
+                RandomCandidate(),
+                RandomCandidate()
+            };
+
+            var repositoryStub = new Mock<ICandidateRepository>();
+            repositoryStub.Setup(repo => repo.GetAllAsync(page, size))
+                .ReturnsAsync(candidates);
+
+            var controller = new CandidatesController(repositoryStub.Object);
+
+            //Act
+            var actionResult = await controller.Get(page, size);
+
+            //ASSERT
+            var result = actionResult.Result as OkObjectResult;
+            var returnedCandidates = result.Value as List<Candidate>;
+            returnedCandidates.Should().BeEquivalentTo(candidates, 
+                option=>option.ComparingByMembers<Candidate>());
+        }
+
+        private Candidate RandomCandidate() => new Candidate
+        {
+            Id = Guid.NewGuid(),
+            Firstname = Guid.NewGuid().ToString(),
+            Lastname = Guid.NewGuid().ToString(),
+            Phone = (new Random()).Next(999999999).ToString(),
+            Email = $"{Guid.NewGuid()}@example.com",
+            CallTimeInterval = Guid.NewGuid().ToString(),
+            Comment = Guid.NewGuid().ToString(),
+            CreatedAt = DateTime.UtcNow,
+            GithubLink = $"https://github.com/{Guid.NewGuid()}",
+            LinkedInProfile = $"https://linkedin.com/in/{Guid.NewGuid()}"
+        };
     }
 }
