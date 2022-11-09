34650c882c5b72cbf564977cb4dda52254c1928b
Zephania Eliah Mundekesye <zmunde5@live.com>
2022-11-09 17:23:13 +0300

    added candidates controller and registered the CandidateRepository as service in the program class

---
+++ Job/Controllers/CandidatesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Job.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidatesController : ControllerBase
+    {
+        public CandidatesController()
+        {
+
+        }
+    }
+}
--- Job/Program.cs
+++ Job/Program.cs
@@ -1,6 +1,10 @@
+using Job.Domains.Candidates;
+using Job.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<ICandidateRepository,CandidateRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
