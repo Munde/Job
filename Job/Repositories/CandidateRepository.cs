07621dc9a2af79d0199a7c7e92604a5bd7607572
Zephania Eliah Mundekesye <zmunde5@live.com>
2022-11-09 17:08:14 +0300

    -extracted the code to read csv to private method to reduce the duplication of codes whenver we need to read data

--- Job/Repositories/CandidateRepository.cs
+++ Job/Repositories/CandidateRepository.cs
@@ -12,7 +12,7 @@ namespace Job.Repositories
 {
     public class CandidateRepository : ICandidateRepository
     {
-        private readonly string _datastore="candidates.csv";
+        private readonly string _datastore = "candidates.csv";
         private readonly string _path;
         private IWebHostEnvironment _environment;
 
@@ -27,7 +27,7 @@ namespace Job.Repositories
             try
             {
                 //create a stream write by providing path and the encoding type 
-                using StreamWriter sw=new(_path, false,new UTF8Encoding(true));
+                using StreamWriter sw = new(_path, false, new UTF8Encoding(true));
                 using CsvWriter cw = new(sw);
 
                 //Write header of the csv using the candidate fields then move to next record
@@ -53,31 +53,29 @@ namespace Job.Repositories
 
         public async ValueTask<IEnumerable<Candidate>> GetAllAsync(int page, int size)
         {
-            try
-            {
-                using StreamReader reader = new(_path, Encoding.Default);
-                using CsvReader csvReader = new CsvReader(reader);
-                csvReader.Configuration.RegisterClassMap<CandidateMap>();
-                var records = csvReader.GetRecords<Candidate>();
-                return await Task.FromResult(records.Skip((page-1)*size).Take(size).ToList());
-            }catch(Exception)
-            {
-                throw;
-            }
+            var records = GetCsvRecords();
+            return await Task.FromResult(records.Skip((page - 1) * size).Take(size).ToList());
         }
 
         public async ValueTask<Candidate> GetAsync(Guid id)
         {
+            var records = GetCsvRecords();
+            var candidate = records.FirstOrDefault(x => x.Id == id);
+            Candidate? result = candidate ?? default;
+            return await Task.FromResult<Candidate>(result);
+        }
+
+
+        private IEnumerable<Candidate> GetCsvRecords()
+        {
             try
             {
                 using StreamReader reader = new(_path, Encoding.Default);
-                using CsvReader csvReader = new CsvReader(reader);
+                using CsvReader csvReader = new(reader);
                 csvReader.Configuration.RegisterClassMap<CandidateMap>();
-                var records = csvReader.GetRecords<Candidate>();
-                var candidate = records.FirstOrDefault(x => x.Id == id);
-                Candidate? result = candidate ?? default;
-                return await Task.FromResult<Candidate>(result);
-            }catch(Exception)
+                return csvReader.GetRecords<Candidate>();
+            }
+            catch (Exception)
             {
                 throw;
             }
